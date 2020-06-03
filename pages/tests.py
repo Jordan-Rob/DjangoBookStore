00472257b375191b57b50d33e9c69c3e56cf6207
jordanrob709 <jordanrob709@gmail.com>
2020-06-03 22:13:02 +0300

    added tests for home page

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class HomePgTests(SimpleTestCase):
+
+    def test_homepg_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepg_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
