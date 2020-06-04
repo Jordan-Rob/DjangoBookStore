6087ac86df44235750a973be1d626c8aef4ef0c1
jordanrob709 <jordanrob709@gmail.com>
2020-06-04 15:55:56 +0300

    added  signup functionalities

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
-    path('', include('pages.urls'))
+    path('', include('users.urls')),
+    path('', include('pages.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+
 # Create your views here.
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
