20c7af738ff15666671a7481feaa096f64b186bc
jordanrob709 <jordanrob709@gmail.com>
2020-06-04 15:10:23 +0300

    added  template & html tests

--- pages/tests.py
+++ pages/tests.py
@@ -14,3 +14,11 @@ class HomePgTests(SimpleTestCase):
     def test_homepg_url_name(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+
+    def test_homepg_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepg_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Home Page')
